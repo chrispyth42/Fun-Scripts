f602efd58825657c603f1611deb2691c0ed413bd
Chris <49737464+chrispyth42@users.noreply.github.com>
2019-06-12 14:59:35 -0700

    Update getNews.py

--- getNews.py
+++ getNews.py
@@ -9,7 +9,7 @@
 #Fox: 				http://feeds.foxnews.com/foxnews/national
 #ABC: 				https://abcnews.go.com/abcnews/usheadlines
 #NYT: 				https://rss.nytimes.com/services/xml/rss/nyt/US.xml
-#Washington Post: 	http://feeds.washingtonpost.com/rss/national
+#Washington Post: 		http://feeds.washingtonpost.com/rss/national
 #USA Today:			http://rssfeeds.usatoday.com/usatodaycomnation-topstories&x=1
 #NBC:				https://www.cnbc.com/id/15837362/device/rss/rss.html
 #CBS:				https://www.cbsnews.com/latest/rss/us
