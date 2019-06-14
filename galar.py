510eb9fe977359bd30ed764c1cea390abe24f477
Chris <49737464+chrispyth42@users.noreply.github.com>
2019-06-14 16:18:12 -0700

    Updated to check if any of my favorite mons are in

--- galar.py
+++ galar.py
@@ -13,10 +13,12 @@ with open('data/pkmnGens.csv',mode='r') as infile:
 	reader = csv.reader(infile)
 	data = dict((rows[0],rows[1]) for rows in reader)
 
-#Generations list
+#Vars
 total = 0
 gens = [0,0,0,0,0,0,0,0]	
 strs = ["","","","","","","",""]
+faves = ["Celebi","Quilava","Murkrow","Swellow","Furfrou","Smeargle","Dunsparce","Infernape"]
+faveStr = ""
 
 #Gets the data
 page = lh.fromstring(requests.get("https://www.serebii.net/swordshield/galarpokedex.shtml").content)
@@ -30,14 +32,24 @@ for mon in mons:
 			gen = int(data[mon]) - 1
 			gens[gen] += 1
 			strs[gen] += mon + "\n"
+			#Checks if any of my favorites has been added yet
+			if mon in faves:
+				faveStr += mon + " has been added!\n"
 		except:
 			gens[7] += 1
 			strs[7] += mon + "\n"
 
 #Prints the data
+print ("-"*30)
 for i in range(8):
 	print ("Generation: " + str(int(i) + 1) )
-	print ("----------------------------------------------------------------")
+	print ("-"*30)
 	print (strs[i])
-	print ("----------------------------------------------------------------")
+	print ("-"*30)
 print("Total pokemon: " + str(total))
+
+if faveStr != "":
+	print(faveStr[:-1])
+else:
+	print("No faves added yet :c")
+print ("-"*30)
